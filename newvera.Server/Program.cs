962d2a71a7d377fd5e951ef372e2d76e4b54598b
salah eddine dik <zettodik@gmail.com>
2025-08-02 00:40:25 +0100

    new manktran

--- newvera.Server/Program.cs
+++ newvera.Server/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container this is new rokki
+// .
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
